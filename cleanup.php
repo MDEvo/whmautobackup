92ddcee51a86d2085e9e8a0145baf523d7499153
Liam Demafelix <liamdemafelix.n@gmail.com>
2016-10-29 13:23:09 +0800

    Add cleanup script

---
+++ cleanup.php
@@ -0,0 +1,36 @@
+<?php
+
+// Get the kernel
+require "kernel.php";
+
+$counter = 0;
+$files = 0;
+
+// List accounts
+foreach ($servers as $server) {
+    $name = $server['name'];
+    unset($server['name']);
+    $$name = new \Gufy\CpanelPhp\Cpanel($server);
+    echo "=======================\nAccounts for {$name}\n=======================\n";
+    $accounts = json_decode($$name->listaccts());
+    foreach ($accounts->acct as $account) {
+        $data = json_decode($$name->execute_action('2', 'Backups', 'listfullbackups', $account->user));
+        $total = count($data->cpanelresult->data);
+        echo "Processing {$total} backup files for {$account->user}...\n";
+        foreach ($data->cpanelresult->data as $file) {
+            // Delete
+            $$name->execute_action('2', 'Fileman', 'fileop', $account->user, ['op' => 'unlink', 'sourcefiles' => $file->file]);
+            echo "\tFile {$file->file} deleted.\n";
+            $files++;
+        }
+        echo "Cleanup for account {$account->user} done.\n\n";
+        $counter++;
+    }
+    echo "\n";
+}
+
+echo "Done. Processed {$counter} accounts and {$files} backup files.";
+
+if (!empty($mail)) {
+    $mg->sendMessage($mail['domain'], ['from' => $mail['from'], 'to' => $mail['to'], 'subject' => 'Cleanup Complete', 'text' => 'Backup cleanup for ' . date("F j, Y g:i A") . ' completed. Backed up ' . $counter . ' accounts and ' . $files . ' files in total.']);
+}
