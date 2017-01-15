680143a4ede2226a11ef782f58c37ecac6c57c53
Liam Demafelix <liamdemafelix.n@gmail.com>
2017-01-15 10:40:35 +0800

    Fix WARN on disabled mail feature setups

--- kernel.php
+++ kernel.php
@@ -6,7 +6,9 @@ require "app/config.php";
 // Get functions
 require "app/functions.php";
 
-require 'vendor/autoload.php';
-use Mailgun\Mailgun;
+if (!empty($mail)) {
+  require 'vendor/autoload.php';
+  use Mailgun\Mailgun;
 
-$mg = new Mailgun($mail['key']);
+  $mg = new Mailgun($mail['key']);
+}
